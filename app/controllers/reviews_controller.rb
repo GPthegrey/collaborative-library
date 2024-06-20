492db757e3f13d28003b568b6afe429f1a19efd5
Guido Palmili <guidopalmili@gmail.com>
2024-06-20 09:41:56 +0200

    added logic to loans and reviews controllers, now reviews are created. Need to: display the comments, limit one comment per user per loan

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,13 @@
 class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
-    @review.user_reviewer = current_user
-    
+    @review.user_reviewer_id = current_user.id
+    loan = Loan.find(review_params[:loan_id])
+    users = [loan.owner_id, loan.borrower_id]
+    reviewed_id = users.reject { |user_id| user_id == current_user.id }.first
+    @review.user_reviewed_id = reviewed_id
     if @review.save
-      redirect_to @review
+      redirect_to loan_path(loan)
     else
       render 'new'
     end
@@ -13,6 +16,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:rating, :comment)
+    params.require(:review).permit(:rating, :comment, :loan_id, :user_reviewed_id)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :user_reviewed
-  belongs_to :user_reviewer
+  belongs_to :user_reviewed, class_name: 'User'
+  belongs_to :user_reviewer, class_name: 'User'
   belongs_to :loan
 end
