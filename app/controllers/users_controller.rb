f3ec244c4f33a2ff3e08bc6130446ae758bfa2d5
Guido Palmili <guidopalmili@gmail.com>
2024-06-20 10:35:48 +0200

    adding review and rating to users show. adding link to see another customers profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = Book.where(user: @user)
+    @reviews = Review.where(user_reviewed_id: @user.id)
+    @rating = Review.where(user_reviewed_id: @user.id).average(:rating)
   end
 
   def profile
